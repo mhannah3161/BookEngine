cea9082116478b8521ee78f6603c9e7feffc2f19
Michael Hannah <Michaelrkhannah@gmail.com>
2023-11-30 14:56:06 -0700

    added main jsx changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,23 +1,39 @@
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import App from './App.jsx'
 import SearchBooks from './pages/SearchBooks'
 import SavedBooks from './pages/SavedBooks'
 
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: (
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    ),
     errorElement: <h1 className='display-2'>Wrong page!</h1>,
     children: [
       {
         index: true,
-        element: <SearchBooks />
+        element: (
+          <ApolloProvider client={client}>
+            <SearchBooks />
+          </ApolloProvider>
+        ),
       }, {
         path: '/saved',
-        element: <SavedBooks />
+        element: (
+          <ApolloProvider client={client}>
+            <SavedBooks />
+          </ApolloProvider>
+        ),
       }
     ]
   }
